bcaa867cb3da1e05a58ee2bc31d468265ff68b7c
Fahri Gunadi <fahri@fahrigunadi.dev>
2024-08-03 15:27:33 +0700

    feat: base64 to file

--- handler/file.go
+++ handler/file.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io"
 	"mime"
+	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -61,5 +64,76 @@ func FileSubmit(c *fiber.Ctx) error {
 
 	return c.Render("file-result", fiber.Map{
 		"Base64": dataURL,
+	})
+}
+
+func Base64ToFile(c *fiber.Ctx) error {
+	return c.Render("base64-to-file", fiber.Map{})
+}
+
+func Base64ToFileSubmit(c *fiber.Ctx) error {
+	payload := struct {
+		Base64 string `json:"base64"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "Base64 is invalid",
+		})
+	}
+	parts := strings.Split(payload.Base64, ",")
+	if len(parts) != 2 {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "invalid base64 format",
+		})
+	}
+
+	mimeParts := strings.Split(parts[0], ":")
+	if len(mimeParts) != 2 {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "invalid MIME type",
+		})
+	}
+
+	mimeType := mimeParts[1]
+	mimeTypeParts := strings.Split(mimeType, "/")
+	if len(mimeTypeParts) != 2 {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "invalid MIME type",
+		})
+	}
+
+	ext := mimeTypeParts[1]
+
+	extParts := strings.Split(ext, ";")
+	ext = extParts[0]
+
+	data, err := base64.StdEncoding.DecodeString(parts[1])
+	if err != nil {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "Base64 is invalid",
+		})
+	}
+
+	filename := fmt.Sprintf("%d.%s", time.Now().UnixNano(), ext)
+	dir := filepath.Join("public", "result")
+	filepath := filepath.Join(dir, filename)
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "Base64 is invalid",
+		})
+	}
+
+	err = os.WriteFile(filepath, data, 0644)
+	if err != nil {
+		return c.Render("base64-to-file-result", fiber.Map{
+			"Base64Err": "Base64 is invalid",
+		})
+	}
+
+	return c.Render("base64-to-file-result", fiber.Map{
+		"Base64": payload.Base64,
+		"Result": filename,
 	})
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/ciptacode/base64-converter/handler"
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +13,9 @@ import (
 )
 
 func main() {
-	LoadEnv()
+	loadEnv()
+
+	createFolderResult()
 
 	engine := html.New("./views", ".html")
 
@@ -20,19 +24,63 @@ func main() {
 		BodyLimit: 10 * 1024 * 1024,
 	})
 
+	app.Static("/", "./public")
+
 	app.Get("/", handler.Text)
 	app.Post("/text", handler.TextSubmit)
 
 	app.Get("/file-to-base64", handler.File)
 	app.Post("/file-to-base64", handler.FileSubmit)
+	app.Get("/base64-to-file", handler.Base64ToFile)
+	app.Post("/base64-to-file", handler.Base64ToFileSubmit)
 
 	app.Listen(":" + os.Getenv("PORT"))
+
+	go func() {
+		for {
+			cleanUpOldFiles("public/result", 5*time.Minute)
+			time.Sleep(1 * time.Minute)
+		}
+	}()
 }
 
-func LoadEnv() {
+func loadEnv() {
 	err := godotenv.Load()
 
 	if err != nil {
 		log.Fatal("Error load .env file")
+	}
+}
+
+func createFolderResult() {
+	dir := filepath.Join("public", "result")
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		log.Printf("Failed to create directory %s: %v", dir, err)
+	}
+}
+
+func cleanUpOldFiles(dir string, maxAge time.Duration) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Printf("Failed to read directory %s: %v", dir, err)
+		return
+	}
+
+	now := time.Now()
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			log.Printf("Failed to get file info %s: %v", file.Name(), err)
+			continue
+		}
+		if now.Sub(info.ModTime()) > maxAge {
+			filePath := filepath.Join(dir, file.Name())
+			if err := os.Remove(filePath); err != nil {
+				log.Printf("Failed to delete file %s: %v", filePath, err)
+			} else {
+				log.Printf("File %s deleted successfully", filePath)
+			}
+		}
 	}
 }
